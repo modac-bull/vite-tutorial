e261e813666c24e9da903382bd222b86329de975
jihoon <ljhljh211@naver.com>
2023-04-05 08:59:17 +0900

    chore: baseUrl 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,47 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
     esbuildOptions: {
-      target: 'es2020',
+      target: "es2020",
     },
   },
   esbuild: {
     // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
-    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+    logOverride: { "this-is-undefined-in-esm": "silent" },
   },
   plugins: [
     react({
       babel: {
         plugins: [
-          'babel-plugin-macros',
+          "babel-plugin-macros",
           [
-            '@emotion/babel-plugin-jsx-pragmatic',
+            "@emotion/babel-plugin-jsx-pragmatic",
             {
-              export: 'jsx',
-              import: '__cssprop',
-              module: '@emotion/react',
+              export: "jsx",
+              import: "__cssprop",
+              module: "@emotion/react",
             },
           ],
           [
-            '@babel/plugin-transform-react-jsx',
-            { pragma: '__cssprop' },
-            'twin.macro',
+            "@babel/plugin-transform-react-jsx",
+            { pragma: "__cssprop" },
+            "twin.macro",
           ],
         ],
       },
     }),
   ],
-})
+  resolve: {
+    alias: [
+      { find: "@", replacement: "./src" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@data", replacement: "/src/data" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@assets", replacement: "/src/assets" },
+    ],
+  },
+});
