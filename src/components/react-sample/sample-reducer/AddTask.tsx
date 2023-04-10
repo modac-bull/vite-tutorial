d60addfd163db0ec02c9ae96c0685a3fe8dbcde3
jihoon <ljhljh211@naver.com>
2023-04-10 16:42:03 +0900

    update: TS 타입 작업중

--- src/components/react-sample/sample-reducer/AddTask.tsx
+++ src/components/react-sample/sample-reducer/AddTask.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export type OnAddTask = (taskText: string) => void;
 type AddTaskProps = {
--- src/pages/react-usecase/SampleUseReducerTwo.tsx
+++ src/pages/react-usecase/SampleUseReducerTwo.tsx
@@ -1,37 +1,136 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 
 import AddTask from "@components/react-sample/sample-reducer/AddTask";
 import TaskList from "@components/react-sample/sample-reducer/TaskList";
 import { TaskType } from "@/components/react-sample/sample-reducer/sample";
 
-export default function SampleUseReducerTwo() {
-  const [tasks, setTasks] = useState(initialTasks);
+// export default function SampleUseReducerTwo() {
+//   const [tasks, setTasks] = useState(initialTasks);
 
-  function handleAddTask(text: string) {
-    setTasks([
-      ...tasks,
-      {
-        id: nextId++,
-        text: text,
-        done: false,
-      },
-    ]);
-  }
+//   function handleAddTask(text: string) {
+//     setTasks([
+//       ...tasks,
+//       {
+//         id: nextId++,
+//         text: text,
+//         done: false,
+//       },
+//     ]);
+//   }
 
-  function handleChangeTask(task: TaskType) {
-    setTasks(
-      tasks.map((t) => {
-        if (t.id === task.id) {
-          return task;
+//   function handleChangeTask(task: TaskType) {
+//     setTasks(
+//       tasks.map((t) => {
+//         if (t.id === task.id) {
+//           return task;
+//         } else {
+//           return t;
+//         }
+//       })
+//     );
+//   }
+
+//   function handleDeleteTask(taskId: number) {
+//     setTasks(tasks.filter((t) => t.id !== taskId));
+//   }
+
+//   return (
+//     <>
+//       <h1>Prague itinerary</h1>
+//       <AddTask onAddTask={handleAddTask} />
+//       <TaskList
+//         tasks={tasks}
+//         onChangeTask={handleChangeTask}
+//         onDeleteTask={handleDeleteTask}
+//       />
+//     </>
+//   );
+// }
+interface AddedAction {
+  type: "added";
+  id: number;
+  text: string;
+}
+interface ChangedAction {
+  type: "changed";
+  id: number;
+  text: string;
+  task: TaskType[];
+}
+interface DeletedAction {
+  type: "deleted";
+  id: number;
+  text: string;
+}
+
+interface DefaultAction {
+  type: null | undefined;
+}
+
+export type TasksAction =
+  | AddedAction
+  | ChangedAction
+  | DeletedAction
+  | DefaultAction;
+
+function tasksReducer(tasks: TaskType[], action: TasksAction) {
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
         } else {
           return t;
         }
-      })
-    );
+      });
+    }
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+}
+
+export default function SampleUseReducerTwo() {
+  // const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  function handleAddTask(text: string) {
+    /* 
+  dispatch({ // action 이라고 함})
+  
+  */
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
   }
 
-  function handleDeleteTask(taskId: number) {
-    setTasks(tasks.filter((t) => t.id !== taskId));
+  function handleChangeTask(task: TaskType) {
+    dispatch({
+      type: "changed",
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
   }
 
   return (
